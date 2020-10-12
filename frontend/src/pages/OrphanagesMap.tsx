3e07e20fbaf157ca6c53a437ba573dfb1df6cbd5
Lucas Sachet <lucasachet@gmail.com>
2020-10-12 13:58:36 -0300

    map API

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarketImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
   return (
@@ -22,8 +24,15 @@ function OrphanagesMap() {
           <span>Rio Grande do Sul</span>
         </footer>
       </aside>
-
-      <div></div>
+        
+      <Map
+        center={[-29.2107106,-51.3587088]}
+        zoom={15}
+        style={{ width:'100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />   */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?acess_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />  
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
