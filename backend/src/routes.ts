48e56be6b56bd1d113111d0bb090e5d9b3bab7f5
Lucas Sachet <lucasachet@gmail.com>
2020-10-13 11:05:35 -0300

    Orphanage details

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ const routes = Router();
 import OrphanagesController from './Controllers/OrphanagesController';
 
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
