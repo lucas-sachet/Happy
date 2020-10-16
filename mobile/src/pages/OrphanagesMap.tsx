178517a46267293416393a0e72369f031e1962e6
Lucas Sachet <lucasachet@gmail.com>
2020-10-16 09:49:35 -0300

    creating registration flow

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import logo from '../images/logo.png';
+
 
 
 export default function OrphanagesMap() {
@@ -13,6 +15,11 @@ export default function OrphanagesMap() {
   function handleNavigateToOrphanageDetails() {
     navigation.navigate('OrphanageDetails');
   }
+  
+  function navigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
+  }
+
   
   return (
     <View style={styles.container}>
@@ -48,9 +55,9 @@ export default function OrphanagesMap() {
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+        <RectButton style={styles.createOrphanageButton} onPress={navigateToCreateOrphanage}>
           <Feather name="plus" size={20} color='#FFF'/>
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   );
